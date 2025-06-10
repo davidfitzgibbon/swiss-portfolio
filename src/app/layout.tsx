006c498866eab29881fb148d4793d9ae007511c8
Alex Trost <19617280+a-trost@users.noreply.github.com>
2025-06-10 14:44:05 -0400

    Add view transition effects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import { ViewTransitions } from "next-view-transitions";
 import { Mona_Sans } from "next/font/google";
+import { BorderTriangles } from "@/components/BorderTriangles";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { Separator } from "@/components/Separator";
 import "./globals.css";
 
 const monaSans = Mona_Sans({
@@ -18,14 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={monaSans.variable}>
-      <body className="font-regular bg-white p-6 text-black antialiased md:text-xl">
-        <Header />
-        <Separator />
-        <main className="mx-auto w-full max-w-5xl">{children}</main>
-        <Separator />
-        <Footer />
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en" className={monaSans.variable}>
+        <body className="font-regular bg-white p-6 text-black antialiased md:text-xl">
+          <Header />
+          <main className="relative mx-auto w-full max-w-5xl py-16">
+            <BorderTriangles />
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
