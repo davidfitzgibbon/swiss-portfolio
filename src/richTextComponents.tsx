fce0d6d608eb4d63c7d9ac6ff679dc3cb3cab5d2
Alex Trost <19617280+a-trost@users.noreply.github.com>
2025-06-10 11:57:44 -0400

    Improve typography with max-width and line height

--- src/richTextComponents.tsx
+++ src/richTextComponents.tsx
@@ -1,6 +1,8 @@
 import { JSXMapSerializer } from "@prismicio/react";
 
 export const components: JSXMapSerializer = {
-  paragraph: ({ children }) => <p className="mb-4 last:mb-0">{children}</p>,
+  paragraph: ({ children }) => (
+    <p className="mb-4 max-w-[70ch] text-lg/8 last:mb-0">{children}</p>
+  ),
   list: ({ children }) => <ul className="list-disc pl-[1em]">{children}</ul>,
 };
